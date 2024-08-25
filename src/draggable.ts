232fe3f41dd6b916666a76a75bb5cd8b743a54b0
Sachin Walunjakar <walunjakarsachin@gmail.com>
2024-08-25 11:30:04 +0530

    corrected types in draggable

--- src/draggable.ts
+++ src/draggable.ts
@@ -7,41 +7,41 @@ var isDragging = false;
  * Emit event for element with `data-drop-target` attribute.
 */
 class Draggable {
-  private e_dragStart: EventEmitter<void> = new EventEmitter<void>();
-  private e_dragMove: EventEmitter<void> = new EventEmitter<void>();
-  private e_dragEnd: EventEmitter<void> = new EventEmitter<void>();
+  private e_dragStart: EventEmitter<MouseEvent> = new EventEmitter<MouseEvent>();
+  private e_dragMove: EventEmitter<MouseEvent> = new EventEmitter<MouseEvent>();
+  private e_dragEnd: EventEmitter<MouseEvent> = new EventEmitter<MouseEvent>();
   constructor() {
     document.addEventListener('mousedown', (event: MouseEvent) => {
       const target = event.target as HTMLElement|null|undefined;
       if (!target?.hasAttribute('data-drop-target')) return;
 
-      this.e_dragStart.emit();
+      this.e_dragStart.emit(event);
       isDragging = true;
     });
 
     document.addEventListener('mousemove', (event: MouseEvent) => {
       if(!isDragging) return;
 
-      this.e_dragMove.emit();
+      this.e_dragMove.emit(event);
     });
 
-    document.addEventListener('mouseup', () => {
+    document.addEventListener('mouseup', (event: MouseEvent) => {
       if(!isDragging) return;
 
-      this.e_dragEnd.emit();
+      this.e_dragEnd.emit(event);
       isDragging = false;
     });
   }
 
-  onDragStart(handler: EventHandler<void>) {
+  onDragStart(handler: EventHandler<MouseEvent>) {
     this.e_dragStart.addListener(handler);
   }
 
-  onDragMove(handler: EventHandler<void>) {
+  onDragMove(handler: EventHandler<MouseEvent>) {
     this.e_dragMove.addListener(handler);
   }
 
-  onDragEnd(handler: EventHandler<void>) {
+  onDragEnd(handler: EventHandler<MouseEvent>) {
     this.e_dragEnd.addListener(handler);
   }
 }
