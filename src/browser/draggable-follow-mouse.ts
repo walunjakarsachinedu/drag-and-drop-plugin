1877c09d7ade2b432a00eb9a22ef35f7ecbf9ef0
Sachin Walunjakar <walunjakarsachin@gmail.com>
2024-08-28 06:51:30 +0530

    bugfix: remove un-necessary swdZones from draggable copy

--- src/browser/draggable-follow-mouse.ts
+++ src/browser/draggable-follow-mouse.ts
@@ -10,7 +10,7 @@ class DraggableFollowMouse {
   addElemCopyToDom(element: HTMLElement|null|undefined) {
     this.draggableCopy = element?.cloneNode(true) as HTMLElement|null|undefined;
     if(!this.draggableCopy) return;
-    delete this.draggableCopy.dataset.swdTargets;
+    delete this.draggableCopy.dataset.swdZones;
     this.draggableCopy.classList.add("draggable-element");
     document.body.appendChild(this.draggableCopy);
   }
