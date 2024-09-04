944e05048b09898ef5c1b46e6bad4b315dc90137
Sachin Walunjakar <walunjakarsachin@gmail.com>
2024-09-04 07:11:04 +0530

    resticted plugin to single finger on touch screen

--- src/browser/swd-mouse.ts
+++ src/browser/swd-mouse.ts
@@ -1,21 +1,26 @@
 import { MouseData, SwdEvent, SwdZoneElmentData } from "../types/types";
 
+type SwdTouch = {target: HTMLElement, identifier: number|null, pageX: number, pageY: number};
+
 class SwdMouse {
+  static touchData?: SwdTouch; 
 
   static addEventListener(type: 'mousedown'|'mousemove'|'mouseup', listener: (ev: SwdEvent) => any) {
-    const normalizedListener = this._convertToNormalListener(listener);
+    const normalizedListener = SwdMouse._convertToNormalListener(listener);
     switch(type) {
       case 'mousedown': 
         document.addEventListener('mousedown', normalizedListener);
-        document.addEventListener('touchstart', normalizedListener);
+        document.addEventListener('touchstart', SwdMouse._onTouchStart(listener));
         break;
       case 'mousemove': 
         document.addEventListener('mousemove', normalizedListener);
-        document.addEventListener('touchmove', normalizedListener);
+        document.addEventListener('touchmove', SwdMouse._onTouchMove(listener));
+        break;
       case 'mouseup': 
         document.addEventListener('mouseup', normalizedListener);
-        document.addEventListener('touchend', normalizedListener);
-        document.addEventListener('touchcancel', normalizedListener); // Fallback for touch cancel
+        document.addEventListener('touchend', SwdMouse._onTouchEnd(listener));
+        document.addEventListener('touchcancel', SwdMouse._onTouchEnd(listener)); // Fallback for touch cancel
+        break;
     }
   }
 
@@ -27,18 +32,19 @@ class SwdMouse {
   }
 
   private static _convertToNormalListener(listener: (ev: SwdEvent) => any) : (ev: TouchEvent|MouseEvent) => any {
-    return (ev: TouchEvent|MouseEvent) => {
-      const data = this._preparekSwdEventData(ev);
+    return (event: TouchEvent|MouseEvent) => {
+      if(!SwdMouse._getTarget(event)) return;
+      const data = SwdMouse._preparekSwdEventData(event);
       listener(data);
     };
   }
 
   private static _preparekSwdEventData(event: MouseEvent|TouchEvent) : SwdEvent {
-    const target = this._getTarget(event);
+    const target = SwdMouse._getTarget(event);
     const height = target.offsetHeight, width = target.offsetHeight;
     const x = target.offsetLeft, y = target.offsetTop;
-    const mouseX = event instanceof MouseEvent ? event.pageX : (event as TouchEvent).changedTouches[0].pageX;
-    const mouseY = event instanceof MouseEvent ? event.pageY : (event as TouchEvent).changedTouches[0].pageY;
+    const mouseX = event instanceof MouseEvent ? event.pageX : SwdMouse.touchData?.pageX!;
+    const mouseY = event instanceof MouseEvent ? event.pageY : SwdMouse.touchData?.pageY!;
     const dx = mouseX-x, dy = mouseY-y;
 
     const swdZoneElement: SwdZoneElmentData = { 
@@ -58,14 +64,69 @@ class SwdMouse {
     if (event instanceof MouseEvent) {
         target = event.target as HTMLElement;
     } else if (event instanceof TouchEvent) {
-        const touch = event.changedTouches[0];
-        if (touch) {
-          target = document.elementFromPoint(touch.pageX, touch.pageY) as HTMLElement;
-        }
+        target = SwdMouse.touchData!.target as HTMLElement;
     }
-
     return target!;
   }
+
+
+  /* ✋ start logic: to restrict single finger touch */
+
+  /** Wrap listener in listener which is accepted touch event listener. */
+  private static _onTouchStart(listener: (ev: SwdEvent) => any) : (ev: TouchEvent) => any {
+    const normalizedListener = SwdMouse._convertToNormalListener(listener);
+    return (touchEvent: TouchEvent) => {
+      const touch = SwdMouse._getTouchEvent(touchEvent.touches);
+      if(!touch) {
+        SwdMouse.touchData = {
+          target: touchEvent.changedTouches[0].target as HTMLElement, 
+          identifier: touchEvent.changedTouches[0].identifier,
+          pageX: touchEvent.changedTouches[0].pageX,
+          pageY: touchEvent.changedTouches[0].pageY,
+        };
+        normalizedListener(touchEvent);
+      }
+    }
+  }
+
+
+  /** Wrap listener in listener which is accepted touch event listener. */
+  private static _onTouchMove(listener: (ev: SwdEvent) => any) : (ev: TouchEvent) => any {
+    const normalizedListener = SwdMouse._convertToNormalListener(listener);
+    return (touchEvent: TouchEvent) => {
+      const touch = this._getTouchEvent(touchEvent.changedTouches);
+      if(touch) {
+        console.log(touch);
+        SwdMouse.touchData!.target = document.elementFromPoint(touch.pageX, touch.pageY) as HTMLElement;
+        SwdMouse.touchData!.pageX = touch.pageX;
+        SwdMouse.touchData!.pageY = touch.pageY;
+        // async with original object
+        normalizedListener(touchEvent);
+      }
+    }
+  }
+
+
+  /** Wrap listener in listener which is accepted touch event listener. */
+  private static _onTouchEnd(listener: (ev: SwdEvent) => any) : (ev: TouchEvent) => any {
+    const normalizedListener = SwdMouse._convertToNormalListener(listener);
+    return (touchEvent: TouchEvent) => {
+      const touch = this._getTouchEvent(touchEvent.changedTouches);
+      if(touch) {
+        SwdMouse.touchData!.identifier = null;
+        normalizedListener(touchEvent);
+      }
+    }
+  }
+
+  private static _getTouchEvent(touchList: TouchList) : Touch|undefined {
+    const touch = [...touchList].find(touch => touch.identifier == SwdMouse.touchData?.identifier);
+    if(touch) return touch;
+    return undefined;
+  }
+
+
+  /* 🛑 end logic: to restrict single finger touch */
 
 }
 
